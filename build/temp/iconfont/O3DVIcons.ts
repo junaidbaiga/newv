235f00c70a2214c3c5ae66182ca6c2cd04520424
junaidbaiga <junaidbaigaleem1@outlook.com>
2022-03-27 15:49:35 +0400

    initial commit

---
+++ build/temp/iconfont/O3DVIcons.ts
@@ -0,0 +1,177 @@
+export type O3DvIconsId =
+  | "arrow_down"
+  | "arrow_left"
+  | "arrow_right"
+  | "arrow_up"
+  | "close"
+  | "collapse"
+  | "deisolate"
+  | "details"
+  | "donate"
+  | "expand"
+  | "export"
+  | "feedback"
+  | "file_download"
+  | "files"
+  | "fit"
+  | "fix_up_off"
+  | "fix_up_on"
+  | "flat_list"
+  | "flip"
+  | "github"
+  | "hidden"
+  | "info"
+  | "isolate"
+  | "materials"
+  | "measure_angle"
+  | "measure_distance_parallel"
+  | "measure_distance"
+  | "measure"
+  | "meshes"
+  | "missing_files"
+  | "model"
+  | "open_url"
+  | "open"
+  | "settings"
+  | "share"
+  | "tree_mesh"
+  | "tree_view"
+  | "twitter"
+  | "up_y"
+  | "up_z"
+  | "visible"
+  | "warning";
+
+export type O3DvIconsKey =
+  | "ArrowDown"
+  | "ArrowLeft"
+  | "ArrowRight"
+  | "ArrowUp"
+  | "Close"
+  | "Collapse"
+  | "Deisolate"
+  | "Details"
+  | "Donate"
+  | "Expand"
+  | "Export"
+  | "Feedback"
+  | "FileDownload"
+  | "Files"
+  | "Fit"
+  | "FixUpOff"
+  | "FixUpOn"
+  | "FlatList"
+  | "Flip"
+  | "Github"
+  | "Hidden"
+  | "Info"
+  | "Isolate"
+  | "Materials"
+  | "MeasureAngle"
+  | "MeasureDistanceParallel"
+  | "MeasureDistance"
+  | "Measure"
+  | "Meshes"
+  | "MissingFiles"
+  | "Model"
+  | "OpenUrl"
+  | "Open"
+  | "Settings"
+  | "Share"
+  | "TreeMesh"
+  | "TreeView"
+  | "Twitter"
+  | "UpY"
+  | "UpZ"
+  | "Visible"
+  | "Warning";
+
+export enum O3DvIcons {
+  ArrowDown = "arrow_down",
+  ArrowLeft = "arrow_left",
+  ArrowRight = "arrow_right",
+  ArrowUp = "arrow_up",
+  Close = "close",
+  Collapse = "collapse",
+  Deisolate = "deisolate",
+  Details = "details",
+  Donate = "donate",
+  Expand = "expand",
+  Export = "export",
+  Feedback = "feedback",
+  FileDownload = "file_download",
+  Files = "files",
+  Fit = "fit",
+  FixUpOff = "fix_up_off",
+  FixUpOn = "fix_up_on",
+  FlatList = "flat_list",
+  Flip = "flip",
+  Github = "github",
+  Hidden = "hidden",
+  Info = "info",
+  Isolate = "isolate",
+  Materials = "materials",
+  MeasureAngle = "measure_angle",
+  MeasureDistanceParallel = "measure_distance_parallel",
+  MeasureDistance = "measure_distance",
+  Measure = "measure",
+  Meshes = "meshes",
+  MissingFiles = "missing_files",
+  Model = "model",
+  OpenUrl = "open_url",
+  Open = "open",
+  Settings = "settings",
+  Share = "share",
+  TreeMesh = "tree_mesh",
+  TreeView = "tree_view",
+  Twitter = "twitter",
+  UpY = "up_y",
+  UpZ = "up_z",
+  Visible = "visible",
+  Warning = "warning",
+}
+
+export const O3_DV_ICONS_CODEPOINTS: { [key in O3DvIcons]: string } = {
+  [O3DvIcons.ArrowDown]: "61697",
+  [O3DvIcons.ArrowLeft]: "61698",
+  [O3DvIcons.ArrowRight]: "61699",
+  [O3DvIcons.ArrowUp]: "61700",
+  [O3DvIcons.Close]: "61701",
+  [O3DvIcons.Collapse]: "61702",
+  [O3DvIcons.Deisolate]: "61703",
+  [O3DvIcons.Details]: "61704",
+  [O3DvIcons.Donate]: "61705",
+  [O3DvIcons.Expand]: "61706",
+  [O3DvIcons.Export]: "61707",
+  [O3DvIcons.Feedback]: "61708",
+  [O3DvIcons.FileDownload]: "61709",
+  [O3DvIcons.Files]: "61710",
+  [O3DvIcons.Fit]: "61711",
+  [O3DvIcons.FixUpOff]: "61712",
+  [O3DvIcons.FixUpOn]: "61713",
+  [O3DvIcons.FlatList]: "61714",
+  [O3DvIcons.Flip]: "61715",
+  [O3DvIcons.Github]: "61716",
+  [O3DvIcons.Hidden]: "61717",
+  [O3DvIcons.Info]: "61718",
+  [O3DvIcons.Isolate]: "61719",
+  [O3DvIcons.Materials]: "61720",
+  [O3DvIcons.MeasureAngle]: "61721",
+  [O3DvIcons.MeasureDistanceParallel]: "61722",
+  [O3DvIcons.MeasureDistance]: "61723",
+  [O3DvIcons.Measure]: "61724",
+  [O3DvIcons.Meshes]: "61725",
+  [O3DvIcons.MissingFiles]: "61726",
+  [O3DvIcons.Model]: "61727",
+  [O3DvIcons.OpenUrl]: "61728",
+  [O3DvIcons.Open]: "61729",
+  [O3DvIcons.Settings]: "61730",
+  [O3DvIcons.Share]: "61731",
+  [O3DvIcons.TreeMesh]: "61732",
+  [O3DvIcons.TreeView]: "61733",
+  [O3DvIcons.Twitter]: "61734",
+  [O3DvIcons.UpY]: "61735",
+  [O3DvIcons.UpZ]: "61736",
+  [O3DvIcons.Visible]: "61737",
+  [O3DvIcons.Warning]: "61738",
+};
